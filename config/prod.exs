48202658cfa4424ae038df1b9cd72d7320e64d46
Thomas de Grivel <thoxdg@gmail.com>
2021-11-29 09:21:04 +0100

    config prod

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :kmxgit, KmxgitWeb.Endpoint,
-  http: [net: :inet, ip: {127, 0, 0, 1}, port: 15008]
+  http: [net: :inet, ip: {127, 0, 0, 1}, port: 15008],
   url: [scheme: "https", host: "git.kmx.io", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
