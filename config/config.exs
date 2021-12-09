8de75bcb70963452d7a2f12ad813146f4800662e
Thomas de Grivel <thoxdg@gmail.com>
2021-12-09 13:35:23 +0100

    favicon. move assets to _assets

--- config/config.exs
+++ config/config.exs
@@ -23,7 +23,7 @@ config :kmxgit, KmxgitWeb.Endpoint,
 config :dart_sass,
   version: "1.39.0",
   default: [
-    args: ~w(css/app.scss ../priv/static/assets/app.css),
+    args: ~w(css/app.scss ../priv/static/_assets/app.css),
     cd: Path.expand("../assets", __DIR__)
   ]
 
@@ -44,7 +44,7 @@ config :esbuild,
   version: "0.12.18",
   default: [
     args:
-      ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+      ~w(js/app.js --bundle --target=es2016 --outdir=../priv/static/_assets --external:/fonts/* --external:/images/*),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
   ]
--- lib/kmxgit_web/endpoint.ex
+++ lib/kmxgit_web/endpoint.ex
@@ -20,7 +20,7 @@ defmodule KmxgitWeb.Endpoint do
     at: "/",
     from: :kmxgit,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: ~w(_assets _fonts _images robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
