62e1a0bac35b8f5e4463e1c50147d76fc823d3c2
Thomas de Grivel <thoxdg@gmail.com>
2022-02-03 11:16:03 +0100

    discord bot

--- lib/discord.ex
+++ lib/discord.ex
@@ -2,7 +2,7 @@ defmodule Discord do
 
   def error(params) do
     channel = Application.get_env(:kmxgit, :discord_errors_channel)
-    url = "/channels/#{channel}/messages"
+    url = "https://discord.com/api/webhooks/938739293336240139/rwlm6amtl8aGWyDt1iZtUa__HISah8KQeNuWD3_UrVTyZjxcAn6UGaZ3EJ0Rq2pt4_5e"
     message = %{content: inspect(params)}
     json = Jason.encode!(message)
     HTTPoison.post(url, json, [{"Content-Type", "application/json"}])
