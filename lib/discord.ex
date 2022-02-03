3ce9dadea48e927a9962eab21c9abd5fa43c6b4d
Thomas de Grivel <thoxdg@gmail.com>
2022-02-03 12:31:00 +0100

    more info in discord errors

--- lib/discord.ex
+++ lib/discord.ex
@@ -1,7 +1,9 @@
 defmodule Discord do
 
-  def error(params) do
-    url = Application.get_env(:kmxgit, :discord_errors_webhook)
+  def error(conn, params) do
+    uri = conn.request_uri
+    user = conn.assigns[:current_user]
+    webhook = Application.get_env(:kmxgit, :discord_errors_webhook)
     reason = if (try do params.reason.message rescue _ -> nil end) do
       type = params.reason.__struct__
       "#{type}: #{params.reason.message}"
@@ -9,8 +11,8 @@ defmodule Discord do
       inspect(params.reason)
     end
     stack = Stack.to_string(params.stack)
-    message = %{content: "```#{params.kind} #{reason}\n\n#{stack}```"}
+    message = %{content: "#{uri}\n#{user.slug.slug}\n```#{params.kind} #{reason}\n\n#{stack}```"}
     json = Jason.encode!(message)
-    HTTPoison.post(url, json, [{"Content-Type", "application/json"}])
+    HTTPoison.post(webhook, json, [{"Content-Type", "application/json"}])
   end
 end
