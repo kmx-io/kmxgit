3c429e229a4525b5ed6e734947053c345918c7ec
Thomas de Grivel <thoxdg@gmail.com>
2023-10-17 10:43:54 +0200

    increase db resources

--- config/runtime.exs
+++ config/runtime.exs
@@ -15,7 +15,8 @@ if config_env() == :prod do
     # ssl: true,
     # socket_options: [:inet6],
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "20"),
+    queue_target: 5000
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
