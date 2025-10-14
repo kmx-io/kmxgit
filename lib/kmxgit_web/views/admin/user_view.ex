0115207591cb98524f7c861bb65665034241749c
Thomas de Grivel <thoxdg@gmail.com>
2025-10-14 15:59:53 +0200

    fix view

--- lib/kmxgit_web/views/admin/user_view.ex
+++ lib/kmxgit_web/views/admin/user_view.ex
@@ -15,7 +15,7 @@ defmodule KmxgitWeb.Admin.UserView do
   use KmxgitWeb, :view
 
   alias Kmxgit.RepositoryManager.Repository
-  alias Kmxgit.UserManager.User
+  alias Kmxgit.UserManager.{Avatar, User}
 
   def page_link(conn, title \\ nil, page) do
     link title || page, to: Routes.admin_user_path(conn, :index, page: page, per: conn.assigns.pagination.per, search: conn.assigns.search, sort: conn.assigns.sort), class: "btn btn-primary"
